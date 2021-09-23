140b9515b2b4c2664d156cdbf9b243a75b441ebe
hatysfriend <hatysfriend@gmail.com>
2021-09-23 15:59:44 +1000

    Updates

---
+++ precache-manifest.f255b66620b2e6f2115118119498c4de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c13c1f528511f386bc6dd1c64670bb",
+    "url": "/unichat/index.html"
+  },
+  {
+    "revision": "1c266ef13adbec7945f1",
+    "url": "/unichat/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "f9b87244a6bf562f00ad",
+    "url": "/unichat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "1c266ef13adbec7945f1",
+    "url": "/unichat/static/js/2.033111ed.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/unichat/static/js/2.033111ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b87244a6bf562f00ad",
+    "url": "/unichat/static/js/main.11eaed5d.chunk.js"
+  },
+  {
+    "revision": "d81067fa5f1d98a40195",
+    "url": "/unichat/static/js/runtime-main.014caeaa.js"
+  }
+]);
